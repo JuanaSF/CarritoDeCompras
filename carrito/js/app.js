c6f61cc00804c60986d1b1424bac83356a5ad4ba
Juana <juana_29_1@hotmail.com>
2020-07-14 20:56:36 -0300

    Se le da la funcionalidad de insertar un curso al carrito. Esto se logra seleccionando un boton con la clase agergar-carrito y luego haciendo  un traversing para llegar a su elemento padre que contiene toda la info del curso, los datos se obtienen con la funcion leerDatosCurso la cual guarda la info en un objeto infoCurso que finalmente lo recibira la funcion insetarCarrito que crea un nuevo elemento html que se inserta en el DOM desde javaScript.

---
+++ carrito/js/app.js
@@ -0,0 +1,60 @@
+//-------------------- Variables ---------------------
+
+const carrito = document.getElementById('carrito');
+const cursos = document.getElementById('lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody');
+
+
+
+
+//-------------------- EventsListeners ------------------
+
+cargarEnventListeners();
+
+function cargarEnventListeners(){
+
+    //Se ejecuta cuando se preciona agregar al carrito
+    cursos.addEventListener('click', comprarCurso);
+}
+
+//-------------------- Funciones ------------------------
+
+//funncion que añade al curso al carrito
+function comprarCurso(e){
+    e.preventDefault();
+
+    //Delegation para agregar al carrito
+    if(e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement;
+        //Envia el curso para leer sus datos
+        leerDatosCurso(curso);
+    }
+}
+
+//Lee los datos del curso
+function leerDatosCurso(curso){
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoCurso);
+}
+
+//Muestra el curso seleccionado en el carrito
+function insertarCarrito(curso){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${curso.imagen}" width=100>
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${curso.id}
+            ">X</a>
+        </td>
+    `;
+    listaCursos.appendChild(row);
+}
